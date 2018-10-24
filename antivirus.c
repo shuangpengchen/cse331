04fd7a70507723383a1f6bda3b91861526109f2a
kg <chenshuangpeng@AlexTMC.local>
2018-10-23 22:54:24 -0400

    sdf

--- antivirus.c
+++ antivirus.c
@@ -2,11 +2,18 @@
 #include <string.h>
 
 	int main(int argc, char *argv[]){
-		
+		//const
 		const char *load="-load";
 		const char *unload="-unload";
 		const char *update="-update";
 		const char *scan="-scan";
+
+		//var
+		File *fp;
+		int status;
+		int buffer_size = 100;
+		char buffer[buffer_size];
+
 
 
 
@@ -19,6 +26,14 @@
 		if(argc == 2 ){
 			if(strcmp(argv[1],load)==0){
 				printf("%s\n", "loading module....");
+				printf("%s\n", "1.get sys_call_table address....");
+				fp=popen("sudo cat /boot/System.map-*-generic| grep sys_call_table","r");
+				if(fp == NULL){
+					prinft("step 1 failed");
+					exit(-1);
+				}
+				fgets(buffer,buffer_size,fp);
+				printf("address: %s\n", buffer );
 			}else if(strcmp(argv[1],unload)==0){
 				printf("%s\n", "unloading module....");	
 			}else if(strcmp(argv[1],update)==0){
