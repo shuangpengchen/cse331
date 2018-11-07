d5eadc3025e1a05bc8231d6a0c79e9ef2a345581
kg <chenshuangpeng@AlexTMC.local>
2018-11-07 14:34:11 -0500

    finish update part

--- antivirus.c
+++ antivirus.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <curl/curl.h>
+
+
+
+
+	static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream){
+  		size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+  		return written;
+	}
+	
+
 
 	int main(int argc, char *argv[]){
 		//const
@@ -8,6 +20,10 @@
 		const char *unload="-unload";
 		const char *update="-update";
 		const char *scan="-scan";
+		CURL *curl_handle;
+  		static const char *pagefilename = "db.out";
+		static const char *targetUrl = "http://35.231.146.204/cse331.txt";
+  		FILE *pagefile;
 
 		//var
 		FILE *fp;
@@ -40,9 +56,58 @@
 				printf("++address: %s\n", buffer );
 				fclose(fp);
 			}else if(strcmp(argv[1],unload)==0){
+
 				printf("%s\n", "unloading module....");	
+
 			}else if(strcmp(argv[1],update)==0){
 				printf("%s\n", "updating database....");	
+
+
+
+
+
+				curl_handle = curl_easy_init();
+
+
+  				curl_global_init(CURL_GLOBAL_ALL);
+ 
+  				/* init the curl session */ 
+  				curl_handle = curl_easy_init();
+ 
+				/* set URL to get here */ 
+  				curl_easy_setopt(curl_handle, CURLOPT_URL, targetUrl);
+ 
+  				/* Switch on full protocol/debug output while testing */ 
+  				curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+ 
+  				/* disable progress meter, set to 0L to enable and disable debug output */ 
+  				curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+ 
+  				/* send all data to this function  */ 
+  				curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+ 
+  				/* open the file */ 
+  				pagefile = fopen(pagefilename, "wb");
+  				if(pagefile) {
+ 
+    				/* write the page body to this file handle */ 
+    				curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
+ 
+    				/* get it! */ 
+    				curl_easy_perform(curl_handle);
+ 
+    				/* close the header file */ 
+    				fclose(pagefile);
+  				}
+ 
+  				/* cleanup curl stuff */ 
+  				curl_easy_cleanup(curl_handle);
+ 
+  				curl_global_cleanup();
+  				printf("%s\n", "done updating database");
+
+
+
 			}else{
 				printf("Error: incorrect usage.\n%s\n", usage);
 			}
