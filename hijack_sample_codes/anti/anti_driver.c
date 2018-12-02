c061063e96a743bafeb08e95602ab5e4bc241d84
kg <chenshuangpeng@AlexTMC.local>
2018-12-02 16:10:52 -0500

    testing module

--- hijack_sample_codes/anti/anti_driver.c
+++ hijack_sample_codes/anti/anti_driver.c
@@ -47,9 +47,9 @@ int set_addr_ro(long unsigned int _addr)
 //   return call_usermodehelper_exec( sub_info, UMH_WAIT_PROC );
 // }
 
- int invoke_user_space_process(const char* arg1,const char *arg2 )
+ int invoke_user_space_process(const char* arg1,const char *arg2 ,const char *arg3 )
 {
- char *argv[] = { arg1, arg2 , NULL };
+ char *argv[] = { arg1, arg2 ,arg3 , NULL };
  char *envp[] = {
        "HOME=/",
        "TERM=linux",
@@ -81,9 +81,9 @@ asmlinkage int
 new_open(const char *filename, int flags, int mode)
 {
     
-    if(flags == 32768 && strstr(filename,"test/cse331/sig") != NULL){
+    if(flags == 32768 && strstr(filename,"test/cse331/sig/kittens/") != NULL){
         //printk(KERN_INFO "----->>>>>> Intercepting open(%s, %d, %d)\n", filename, flags, mode);
-        invoke_user_space_process("/usr/bin/logger",filename);
+        invoke_user_space_process("/bin/anti","-scan",filename);
         if(strstr(buffer,filename)!=NULL){
             printk(KERN_ALERT "file is bad, stop opening it..");
             return;
@@ -98,7 +98,6 @@ static int __init
 init(void)
 {
     printk(KERN_INFO "++++++++++++ ANTI PROJECT INIT FUNC ++++++++++++\n");
-    invoke_user_space_process("/usr/bin/logger","init");
     int result;
     result = register_chrdev(ANTI_MAJOR, "anti", &anti_fops);
     if (result < 0){ // fail to register device
@@ -117,7 +116,6 @@ static void __exit
 cleanup(void)
 {
     printk(KERN_INFO "------------ ANTI PROJECT EXIT FUNC ------------\n");
-    invoke_user_space_process("/usr/bin/logger","exit");
     unregister_chrdev(ANTI_MAJOR, "anti");
     memset(buffer, 0, sizeof buffer);
     sys_call_table[__NR_open] = old_open;
